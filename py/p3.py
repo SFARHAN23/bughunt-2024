c57acd0a3f483a76f92fbbb8ceb041fa499a1b70
SFARHAN23 <157383151+SFARHAN23@users.noreply.github.com>
2024-03-16 09:51:36 +0530

    Update p3.py

--- py/p3.py
+++ py/p3.py
@@ -2,11 +2,9 @@
 #eg: [2, 4, 6, 17, 10] -> [17, 10] 
 
 def delete_starting_evens(list):
-  for item in list:
-    if list[0] % 2 == 0:
-      list.pop(0)
-    else:
-      break
+  for item in range(len(list),-1,-1): # changing to range 
+    if list[item]%2==0:              # eheck element at index is even
+      list.pop(item)
   return list
 
 list = [2, 8, 10, 11]

2cd1c421d2558f13739c842a0a37e5b133641aa1
SFARHAN23 <157383151+SFARHAN23@users.noreply.github.com>
2024-03-16 09:31:44 +0530

    Update p1.py

--- py/p1.py
+++ py/p1.py
@@ -7,6 +7,8 @@
 def asterisk_tree(height,level):
   if level > height:
     return
+  for i in range(1,height-level+1): #for loop for adding spaces to centre the pattern
+    print(" ",end="")
   for j in range(2*level-1):
     print("*", end="")
   print()

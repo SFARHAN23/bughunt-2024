654693636e09ef36ab6c9058da40f36f4144b344
Kishorekb2003 <132202728+Kishorekb2003@users.noreply.github.com>
2024-03-16 02:06:46 -0400

    Update p3.cpp

--- c/p3.cpp
+++ c/p3.cpp
@@ -14,7 +14,7 @@ vector<int> productExceptSelf(vector<int>& nums) {
     int r = 1;
     for(int i=nums.size()-1;i>=0;i--){
         ans[i] *= r;
-        r = nums[i];
+        r = r*nums[i];
     }
     return ans;
 }
